ec73aad1e89f6b3601f1688d6ea3fe358e4601b0
Geethaikannan <keethairoy@gmail.com>
2026-02-03 01:19:50 +0530

    Initial commit

---
+++ create_dirs.bat
@@ -0,0 +1,8 @@
+md NIDS_Project
+md NIDS_Project\datasets
+md NIDS_Project\scripts
+md NIDS_Project\diagrams
+md NIDS_Project\report
+md NIDS_Project\report\figures
+md NIDS_Project\presentation
+md NIDS_Project\presentation\figures
